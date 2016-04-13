a5f587917a035502b00dc2611fc14f8dac8de119
sanjay <sanjay.singh@3pillarglobal.com>
2016-04-13 21:17:03 +0530

    added processBuilder.commad() test code

--- JacksSpringTriggers/src/test/java/com/codiscope/jaks/triggers/java/commandInjection/CIGITAL_COMMAND_INJECTION_EXEC.java
+++ JacksSpringTriggers/src/test/java/com/codiscope/jaks/triggers/java/commandInjection/CIGITAL_COMMAND_INJECTION_EXEC.java
@@ -1,14 +1,8 @@
 package com.codiscope.jaks.triggers.java.commandInjection;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Enumeration;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 
@@ -35,19 +29,25 @@ public class CIGITAL_COMMAND_INJECTION_EXEC {
 	HttpServletRequest request = null;
 	Runtime rt = Runtime.getRuntime();
 	ProcessBuilder pb;
+	
+
 	public void testWeb() throws IOException {
 		// rt.exec(websource.method1());
 		rt.exec(webMethod());
 	}
 	
 	public void testWebProcessBuilder() throws IOException {
+	
 		pb = new ProcessBuilder(webMethod(), "myArg1", "myArg2");
-		
+				
 		pb = new ProcessBuilder(webMethod2(), "myArg1", "myArg2");
 		
 		pb = new ProcessBuilder(webMethod3(), "myArg1", "myArg2");
 		
 		pb = new ProcessBuilder(webMethod4(), "myArg1", "myArg2");
+		
+		pb.command(webMethod()); // command taint
+		
 	}
 	public String webMethod() {
 		String s01 = request.getRemoteHost();
